1f1a6533a4974503d91d4dce7966b20d5656a99d
Daniel Carvalho <danielscarvalho@gmail.com>
2018-05-16 20:17:57 -0300

    Minor fixes...

--- src/br/edu/cefsa/ftt/ec/Joystick.java
+++ src/br/edu/cefsa/ftt/ec/Joystick.java
@@ -10,7 +10,12 @@ import net.java.games.input.Component.Identifier;
 // https://github.com/jinput/jinput
 // http://www.bartneck.de/2017/08/14/tutorial-on-how-to-install-and-setup-jinput-on-mac-os-x-using-eclipse/
 // http://www.gametutorial.net/article/JInput-Joystick-Test
-// 
+//
+// TODO: Criar uma class para obter os dados do Joystick
+// TODO: Criar uma interface em JavaFX para exibir os dados do controle graficamente
+// TODO: Criar uma ferramenta de desenho com canvas utilizando o Joystick
+// TODO: Criar um jogo 2D ou uma bateria eletrônica com MIDI utilizando o Joystick
+
 
 public class Joystick {
 
@@ -19,98 +24,98 @@ public class Joystick {
 		System.out.println("Out of controll...");
 		
 		Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
-		
-		System.out.println(controllers[11]);
-		
+				
 		for (int i = 0; i < controllers.length; i++) {
-			System.out.println(i + " "+ controllers[i]);
+			System.out.println(i + " " + controllers[i]);
 		} //for
 	
-		 Controller controller = controllers[controllers.length-1]; //Pega o último controle
+		Controller controller = controllers[controllers.length-1]; //Pega o último controle
 		 
-		 System.out.println(controller.getName() + " - " + controller.getType().toString() + " type");
+		System.out.println(controller.getName() + " - " + controller.getType().toString() + " type");
 		 
-		 Component[] components = controller.getComponents();
+		Component[] components = controller.getComponents();
 		 
-		 for (int i = 0; i < components.length; i++)
-			 System.out.println("Componente do Joystick: " + components[i]);
+		for (int i = 0; i < components.length; i++)
+			System.out.println("Componente do Joystick: " + components[i]);
 		 
-		 Boolean loop = true;
-		 while(loop) {
+		Boolean loop = true;
+		while(loop) {
 			 
-			 if (!controller.poll()) {
-				 System.out.println("Fui...");
-				 loop = false;
-			 } else {
+			if (!controller.poll()) {
+				System.out.println("Fui...");
+				loop = false;
+			} else {
 				 
-			/////////////////////////////////////////////////////////////////////////	 
-		    //The magic is here...
+				/////////////////////////////////////////////////////////////////////////	 
+				//The magic is here...
 				 
-				 
-		            for(int i=0; i < components.length; i++) {
-		                Component component = components[i];
-		                Identifier componentIdentifier = component.getIdentifier();
+				for(int i=0; i < components.length; i++) {
+					
+					Component component = components[i];
+		            
+					Identifier componentIdentifier = component.getIdentifier();
 		              
-		                // Buttons
-		                //if(component.getName().contains("Button")){ // If the language is not english, this won't work.
-		                if(componentIdentifier.getName().matches("^[0-9]*$")) { // If the component identifier name contains only numbers, then this is a button.
+		            // Buttons
+		            // If the language is not english, this won't work...
+		            if(componentIdentifier.getName().matches("^[0-9]*$") &
+		            		component.getName().contains("Botão")) { // If the component identifier name contains only numbers, then this is a button.
 		                    
-		                    if (component.getPollData() != 0.0f) {                    
-		                    	String compName = component.getIdentifier().toString();
-		                    	System.out.println(compName);
-		                    } //if
+		                if (component.getPollData() != 0.0f) {                    
+		                   	System.out.println(componentIdentifier);
+		                } //if
 		                    
 		                    // We know that this component was button so we can skip to next component.
 		                    continue;
-		                } //if - buttons
-		                
-		                // Hat switch
-		                if(componentIdentifier == Component.Identifier.Axis.POV){
-		                    float hatSwitchPosition = component.getPollData();
-		                    System.out.println(hatSwitchPosition);
+		            } //if - buttons
+		               
+		            // Hat switch
+		            if(componentIdentifier == Component.Identifier.Axis.POV){
+		                float hatSwitchPosition = component.getPollData();
+		                System.out.println(hatSwitchPosition);
 		                    
-		                    // We know that this component was hat switch so we can skip to next component.
-		                    continue;
-		                } // if hat
+		                // We know that this component was hat switch so we can skip to next component.
+		                continue;
+		            } // if hat
 		             
-		                // Axes
-		                if(component.isAnalog()) {
-		                    float axisValue = component.getPollData();
-		                    //System.out.println(axisValue); //calcular %...
+		            // Axes
+		            if(component.isAnalog()) {
+		                float axisValue = component.getPollData(); //calcular %...
 		                    
-		                    // X axis
-		                    if(componentIdentifier == Component.Identifier.Axis.X) {
-		                        System.out.println("x: " + axisValue);
-		                        continue; // Go to next component.
-		                    }
-		                    // Y axis
-		                    if(componentIdentifier == Component.Identifier.Axis.Y) {
-		                    	System.out.println("y: " + axisValue);
-		                        continue; // Go to next component.
-		                    }
-		                    // Z axis
-		                    if(componentIdentifier == Component.Identifier.Axis.Z) {
-		                    	System.out.println("z: " + axisValue);
-		                        continue; // Go to next component.
-		                    }
-		                    		                    		                    
-		                } //if - axes
+		                // X axis
+		                if(componentIdentifier == Component.Identifier.Axis.X) {
+		                    System.out.println("x: " + axisValue);
+		                    continue; // Go to next component.
+		                }
 		                
-		            }	//for			 
-		 
+		                // Y axis
+		                if(componentIdentifier == Component.Identifier.Axis.Y) {
+		                 	System.out.println("y: " + axisValue);
+		                    continue; // Go to next component.
+		                }
+		                
+		                // Z axis
+		                if(componentIdentifier == Component.Identifier.Axis.Z) {
+		                  	System.out.println("z: " + axisValue);
+		                    continue; // Go to next component.
+		                }
+		                    		                    		                    
+		            } //if - axes
+		                
+		        } //for			 
 				 
-			/////////////////////////////////////////////////////////////////////////	 
-			 } //if
+				/////////////////////////////////////////////////////////////////////////	 
+			 
+			} //if
 				 
-             try {
-				Thread.sleep(30);
-			 } catch (InterruptedException e) {
+            try {
+            	Thread.sleep(30);
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			 } //try
+			} //try
              
-		 } //while - infinity loop
+		} //while - infinity loop
 			 
-	}	//main
+	} //main
 
 } //Joystick
