fa2c6f7efbde6b2cb2cf9ba7c9d92aa2bc829969
Daniel Carvalho <danielscarvalho@gmail.com>
2018-05-17 00:32:53 -0300

    + references...

--- src/br/edu/cefsa/ftt/ec/Joystick.java
+++ src/br/edu/cefsa/ftt/ec/Joystick.java
@@ -11,9 +11,10 @@ import net.java.games.input.Component.Identifier;
 // https://fivedots.coe.psu.ac.th/~ad/jg2/ch11/GamePadIntro.pdf - Java Prog. Techniques for Games. Chapter 28.9. Game Pad
 // http://www.bartneck.de/2017/08/14/tutorial-on-how-to-install-and-setup-jinput-on-mac-os-x-using-eclipse/
 // http://www.gametutorial.net/article/JInput-Joystick-Test
+// https://mvnrepository.com/artifact/net.java.jinput/jinput/2.0.5
 //
 // TODO: Criar uma class para obter os dados do Joystick
-// TODO: Criar uma interface em JavaFX para exibir os dados do controle graficamente
+// TODO: Criar uma interface (UI) em JavaFX para exibir os dados do controle graficamente
 // TODO: Criar uma ferramenta de desenho com canvas utilizando o Joystick
 // TODO: Criar um jogo 2D ou uma bateria eletrônica com MIDI utilizando o Joystick
 
@@ -107,14 +108,14 @@ public class Joystick {
 				/////////////////////////////////////////////////////////////////////////	 
 			 
 			} //if
-				 
+				    
             try {
             	Thread.sleep(30);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} //try
-             
+          
 		} //while - infinity loop
 			 
 	} //main
